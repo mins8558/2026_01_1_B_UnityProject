09496fc3896148c9c8a314bb1a2d1cd8ced71eb8
mins8558 <mins8558@gmail.com>
2026-04-07 11:53:20 +0900

    [Feat] For 문을 활용한 바닥 생성, 한글 폰트 UI 적용

---
+++ Assets/CubeGameUI.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CubeGameUI : MonoBehaviour
+{
+    public TextMeshProUGUI TimerText;
+    public float Timer;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        TimerText.text = "생존시간 : " + Timer.ToString("0,00");
+    }
+}
---
+++ Assets/CubeGenerator.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CubeGenerator : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public int totalCubes = 10;
+    public float cubeSpacing = 1.0f;
+
+    void Start()
+    {
+        GenCube()
+    }
+
+    public void GenCube()
+    {
+        Vector3 myPosition = transform.position;
+        GameObject firstCube = Intantiate(cubePrefab, myPosition, Quaternion.identity);
+
+        for(int i = 1; i < totalCubes; i++)
+        {
+
+            Vector3 position = new Vector3(myPosition.x, myPosition.y, myPosition.z + (i * cubeSpacing));
+            Instantiate(cubePrefab, position, Quaternion.identity);
+        }
+    }
+}
